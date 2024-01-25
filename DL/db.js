11e83b9c9fc1e0ee77dba765b36d4406884f4664
itay weinberg <itayweinberg@itays-MacBook-Air.local>
2024-01-25 17:05:10 +0200

    adjusting mongodb uri

--- DL/db.js
+++ DL/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const MONGO_URL = process.env.MONGO_URL;
+const MONGO_URL = process.env.MONGODB_URI;
 const connectToMongo = async () => {
   try {
     mongoose.set("strictQuery", false);
