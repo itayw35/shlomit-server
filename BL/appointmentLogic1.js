e717e0a5c4e87b4471e1278aab90b11a3c0c12ae
itay weinberg <itayweinberg@itays-MacBook-Air.local>
2024-03-28 17:53:49 +0200

    addin AWS sns service

--- BL/appointmentLogic1.js
+++ BL/appointmentLogic1.js
@@ -1,6 +1,6 @@
 const appointmentController = require("../DL/controllers/appointmentController");
 const axios = require("axios");
-const nodemailer = require("nodemailer")
+const AWS = require("aws-sdk");
 async function getAllAppointments() {
   const today = new Date();
   const appointments = await appointmentController.read(
@@ -23,8 +23,8 @@ async function getAllAppointmentsWithInfo() {
   return { code: 200, message: appointments };
 }
 async function setAppointment(data) {
-  const { name, phone, email ,date } = data;
-  if (!name || !phone || !email || !date) throw { code: 400, message: "missing details" };
+  const { name, phone, date } = data;
+  if (!name || !phone || !date) throw { code: 400, message: "missing details" };
   const isOccupied = await appointmentController.readOne({ time: date });
   if (isOccupied) throw { code: 400, message: "מצטערים, התור תפוס" };
   const formattedDate = new Date(date);
@@ -42,18 +42,15 @@ async function setAppointment(data) {
   appointmentController.create({
     patientName: name,
     phoneNumber: phone,
-    email: email,
     time: timestamp,
   });
-  sendEmail(process.env.MAIL_SENDER_USER_NAME, "זימון תור", `מעוניינת לקבוע תור לתאריך ${date.slice(0, 10)} בשעה ${
-    formattedDate.getHours() + 2
-  }:${formattedDate.getMinutes()}0  ${name}`)
+  sendSMS(`${name} מעוניינ/ת לקבוע תור ב${date}`);
   return {
     code: 200,
     message: `התור שקבעת לתאריך ${date.slice(0, 10)} בשעה ${
       formattedDate.getHours() + 2
     }:${formattedDate.getMinutes()}0 ממתין לאישור. נודיע לך כשהוא יאושר. יום טוב :-)
-  `,
+      `,
   };
 }
 async function updateAppointment(data) {
@@ -63,40 +60,56 @@ async function updateAppointment(data) {
   await appointmentController.update({ _id: id }, { $set: { status: status } });
   return { code: 200, message: `appointment ${status}` };
 }
-async function sendEmail(recieverMail, mailSubject,mailText){
+async function sendEmail(recieverMail, mailSubject, mailText) {
   let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
       user: process.env.MAIL_SENDER_USER_NAME,
-      password: process.env.MAIL_SENDER_PASSWORD
-    }
-  })
+      password: process.env.MAIL_SENDER_PASSWORD,
+    },
+  });
   let mailOptions = {
     from: `"Admin" ${process.env.MAIL_SENDER_USER_NAME}`,
-    to:recieverMail,
-    subject:mailSubject,
-    text: mailText
-  }
-  transporter.sendMail(mailOptions,
-    (error,info)=> {
-      if(error){
-        return console.log(error);
-      }
-      console.log("Message sent: %s", info.messageId);
-    })
+    to: recieverMail,
+    subject: mailSubject,
+    text: mailText,
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message sent: %s", info.messageId);
+  });
 }
 async function sendSMS(phoneNumber, message) {
-  const url = "https://textbelt.com/text";
-  const params = new URLSearchParams();
-  params.append("phone", phoneNumber);
-  params.append("message", message);
-  params.append("key", "textbelt");
-  try {
-    const response = await axios.post(url, params);
-    console.log(response.data);
-  } catch (err) {
-    console.error("Error sending sms: ", err);
-  }
+  AWS.config.update({ region: "eu-north-1" });
+  const credentials = new AWS.Credentials({
+    accessKeyId: process.env.SNS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.SNS_ACCESS_KEY,
+  });
+  AWS.config.credentials = credentials;
+  const params = {
+    Message: message,
+    PhoneNumber: phoneNumber,
+  };
+  SNS.publish(params, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("success", data);
+    }
+  });
+  //   const url = "https://textbelt.com/text";
+  // const params = new URLSearchParams();
+  // params.append("phone", phoneNumber);
+  // params.append("message", message);
+  // params.append("key", "textbelt");
+  // try {
+  //   const response = await axios.post(url, params);
+  //   console.log(response.data);
+  // } catch (err) {
+  //   console.error("Error sending sms: ", err);
+  // }
 }
 module.exports = {
   getAllAppointments,
--- DL/models/appointment.js
+++ DL/models/appointment.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const appointmentSchema = new mongoose.Schema({
   patientName: { type: String, required: true },
   phoneNumber: { type: String, required: true },
-  email: {type: String, required : true},
   time: { type: Date, required: true },
   status: {
     type: String,
